790f6179051f14f024ea17992f4e9866720da8e8
Philip Withnall <withnall@endlessm.com>
2018-01-12 11:10:26 +0000

    libmogwai-schedule: Add some FIXME comments to the D-Bus interfaces
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T20423

--- libmogwai-schedule/schedule-entry-interface.h
+++ libmogwai-schedule/schedule-entry-interface.h
@@ -29,6 +29,9 @@ G_BEGIN_DECLS
 
 /*
  * Declaration of the com.endlessm.DownloadManager1.ScheduleEntry D-Bus interface.
+ *
+ * FIXME: Ideally, there would be a gdbus-codegen mode to generate just this
+ * interface info, because writing it out in C is horrific.
  */
 
 static const GDBusMethodInfo schedule_entry_interface_remove =
