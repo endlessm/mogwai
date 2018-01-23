ccee17f0926795820348e40a9844f64e30df6d38
Philip Withnall <withnall@endlessm.com>
2018-01-23 16:07:15 +0000

    tariff: Add unit tests
    These give high, but not branchwise-perfect, coverage of the
    libmogwai-tariff code. FIXMEs are in place to describe the places which
    still need more thorough testing.
    
    Debian packaging will follow subsequently.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T20750

---
+++ libmogwai-tariff/tests/tariff-loader.c
@@ -0,0 +1,231 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright © 2018 Endless Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Authors:
+ *  - Philip Withnall <withnall@endlessm.com>
+ */
+
+#include "config.h"
+
+#include <glib.h>
+#include <gio/gio.h>
+#include <libmogwai-tariff/tariff-loader.h>
+#include <locale.h>
+
+
+/* Test that get_tariff() returns %NULL if nothing’s been loaded. */
+static void
+test_tariff_loader_unloaded (void)
+{
+  g_autoptr(MwtTariffLoader) loader = NULL;
+  loader = mwt_tariff_loader_new ();
+
+  g_assert_null (mwt_tariff_loader_get_tariff (loader));
+}
+
+/* Test the tariff loader handles erroneous files gracefully. */
+static void
+test_tariff_loader_errors (void)
+{
+  const struct
+    {
+      const guint8 *data;
+      gsize length;
+    }
+  vectors[] =
+    {
+      /* Empty file */
+      { (const guint8 *) "", 0 },
+      /* Incorrect magic */
+      { (const guint8 *) "\x4c\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                         "\x00\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                         "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                         "\x74\x71\x75\x74\x29\x29\x0e", 46 },
+      /* Incorrect magic (not nul terminated) */
+      { (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                         "\x01\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                         "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                         "\x74\x71\x75\x74\x29\x29\x0e", 46 },
+      /* Incorrect version number */
+      { (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                         "\x00\xf1\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                         "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                         "\x74\x71\x75\x74\x29\x29\x0e", 46 },
+      /* Outer variant not in normal form (final byte changed) */
+      { (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                         "\x00\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                         "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                         "\x74\x71\x75\x74\x29\x29\x00", 46 },
+      /* Inner variant type not valid */
+      { (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                         "\x00\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                         "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                         "\x74\x71\x75\xff\x29\x29\x0e", 46 },
+      /* Inner variant invalid (name not nul terminated) */
+      { (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                         "\xff\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                         "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                         "\x74\x71\x75\x74\x29\x29\x0e", 46 },
+      /* Outer variant not valid (truncated) */
+      { (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                         "\x00\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                         "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                         "\x74", 40 },
+      /* Inner variant type valid but not correct for V1 of the format */
+      { (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                         "\x00\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                         "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                         "\x78\x71\x75\x74\x29\x29\x0e", 46 },
+      /* FIXME: Test validation of the tariff and period properties. Maybe do
+       * that using a GVariant? */
+    };
+
+  g_autoptr(MwtTariffLoader) loader = NULL;
+  loader = mwt_tariff_loader_new ();
+
+  for (gsize i = 0; i < G_N_ELEMENTS (vectors); i++)
+    {
+      g_autoptr(GError) error = NULL;
+      g_autoptr(GBytes) bytes = g_bytes_new_static (vectors[i].data, vectors[i].length);
+
+      gboolean retval = mwt_tariff_loader_load_from_bytes (loader, bytes, &error);
+      g_assert_error (error, MWT_TARIFF_ERROR, MWT_TARIFF_ERROR_INVALID);
+      g_assert_false (retval);
+      g_assert_null (mwt_tariff_loader_get_tariff (loader));
+    }
+}
+
+/* Test loading a simple tariff from bytes works. */
+static void
+test_tariff_loader_simple_bytes (void)
+{
+  const guint8 *data =
+    (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66\x00"
+                     "\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66\x66\x00"
+                     "\x00\x00\x00\x00\x00\x7a\x49\x5a\x00\x00\x00\x00\x80\x58"
+                     "\x72\x5a\x00\x00\x00\x00\x04\x00\x00\x00\x01\x00\x00\x00"
+                     "\x00\x94\x35\x77\x00\x00\x00\x00\x80\x11\x50\x5a\x00\x00"
+                     "\x00\x00\x80\xb4\x52\x5a\x00\x00\x00\x00\x03\x00\x00\x00"
+                     "\x01\x00\x00\x00\xff\xff\xff\xff\xff\xff\xff\xff\x0c\x00"
+                     "\x28\x73\x61\x28\x74\x74\x71\x75\x74\x29\x29\x0e";
+  const gsize len = 110;
+
+  g_autoptr(GBytes) bytes = g_bytes_new_static (data, len);
+  g_autoptr(MwtTariffLoader) loader = mwt_tariff_loader_new ();
+  g_autoptr(GError) error = NULL;
+
+  gboolean retval = mwt_tariff_loader_load_from_bytes (loader, bytes, &error);
+  g_assert_no_error (error);
+  g_assert_true (retval);
+
+  MwtTariff *tariff = mwt_tariff_loader_get_tariff (loader);
+  g_assert_true (MWT_IS_TARIFF (tariff));
+
+  /* Check properties. */
+  g_assert_cmpstr (mwt_tariff_get_name (tariff), ==, "test-tariff");
+
+  GPtrArray *periods = mwt_tariff_get_periods (tariff);
+  g_assert_cmpuint (periods->len, ==, 2);
+
+  g_autoptr(GDateTime) period1_start_expected = g_date_time_new_utc (2018, 1, 1, 0, 0, 0);
+  g_autoptr(GDateTime) period1_end_expected = g_date_time_new_utc (2018, 2, 1, 0, 0, 0);
+
+  MwtPeriod *period1 = g_ptr_array_index (periods, 0);
+  GDateTime *period1_start = mwt_period_get_start (period1);
+  GDateTime *period1_end = mwt_period_get_end (period1);
+  g_assert_true (g_date_time_equal (period1_start, period1_start_expected));
+  g_assert_true (g_date_time_equal (period1_end, period1_end_expected));
+  g_assert_cmpint (mwt_period_get_repeat_type (period1), ==, MWT_PERIOD_REPEAT_MONTH);
+  g_assert_cmpuint (mwt_period_get_repeat_period (period1), ==, 1);
+  g_assert_cmpuint (mwt_period_get_capacity_limit (period1), ==, 2 * 1000 * 1000 * 1000);
+
+  g_autoptr(GDateTime) period2_start_expected = g_date_time_new_utc (2018, 1, 6, 0, 0, 0);
+  g_autoptr(GDateTime) period2_end_expected = g_date_time_new_utc (2018, 1, 8, 0, 0, 0);
+
+  MwtPeriod *period2 = g_ptr_array_index (periods, 1);
+  GDateTime *period2_start = mwt_period_get_start (period2);
+  GDateTime *period2_end = mwt_period_get_end (period2);
+  g_assert_true (g_date_time_equal (period2_start, period2_start_expected));
+  g_assert_true (g_date_time_equal (period2_end, period2_end_expected));
+  g_assert_cmpint (mwt_period_get_repeat_type (period2), ==, MWT_PERIOD_REPEAT_WEEK);
+  g_assert_cmpuint (mwt_period_get_repeat_period (period2), ==, 1);
+  g_assert_cmpuint (mwt_period_get_capacity_limit (period2), ==, G_MAXUINT64);
+}
+
+/* Test that loading a tariff with no periods fails gracefully. */
+static void
+test_tariff_loader_empty (void)
+{
+  const guint8 *data =
+    (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                     "\x00\x01\x00\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                     "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                     "\x74\x71\x75\x74\x29\x29\x0e";
+  const gsize len = 46;
+
+  g_autoptr(GBytes) bytes = g_bytes_new_static (data, len);
+  g_autoptr(MwtTariffLoader) loader = mwt_tariff_loader_new ();
+  g_autoptr(GError) error = NULL;
+
+  gboolean retval = mwt_tariff_loader_load_from_bytes (loader, bytes, &error);
+  g_assert_error (error, MWT_TARIFF_ERROR, MWT_TARIFF_ERROR_INVALID);
+  g_assert_false (retval);
+
+  MwtTariff *tariff = mwt_tariff_loader_get_tariff (loader);
+  g_assert_null (tariff);
+}
+
+/* Test that loading a byteswapped tariff with no periods also fails. */
+static void
+test_tariff_loader_empty_byteswapped (void)
+{
+  const guint8 *data =
+    (const guint8 *) "\x4d\x6f\x67\x77\x61\x69\x20\x74\x61\x72\x69\x66\x66"
+                     "\x00\x00\x01\x74\x65\x73\x74\x2d\x74\x61\x72\x69\x66"
+                     "\x66\x00\x00\x00\x00\x00\x0c\x00\x28\x73\x61\x28\x74"
+                     "\x74\x71\x75\x74\x29\x29\x0e";
+  const gsize len = 46;
+
+  g_autoptr(GBytes) bytes = g_bytes_new_static (data, len);
+  g_autoptr(MwtTariffLoader) loader = mwt_tariff_loader_new ();
+  g_autoptr(GError) error = NULL;
+
+  gboolean retval = mwt_tariff_loader_load_from_bytes (loader, bytes, &error);
+  g_assert_error (error, MWT_TARIFF_ERROR, MWT_TARIFF_ERROR_INVALID);
+  g_assert_false (retval);
+
+  MwtTariff *tariff = mwt_tariff_loader_get_tariff (loader);
+  g_assert_null (tariff);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  setlocale (LC_ALL, "");
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/tariff-loader/unloaded", test_tariff_loader_unloaded);
+  g_test_add_func ("/tariff-loader/errors", test_tariff_loader_errors);
+  g_test_add_func ("/tariff-loader/simple/bytes", test_tariff_loader_simple_bytes);
+  g_test_add_func ("/tariff-loader/empty", test_tariff_loader_empty);
+  g_test_add_func ("/tariff-loader/empty/byteswapped",
+                   test_tariff_loader_empty_byteswapped);
+
+  return g_test_run ();
+}
---
+++ libmogwai-tariff/tests/tariff.c
@@ -0,0 +1,147 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright © 2018 Endless Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Authors:
+ *  - Philip Withnall <withnall@endlessm.com>
+ */
+
+#include "config.h"
+
+#include <glib.h>
+#include <gio/gio.h>
+#include <libmogwai-tariff/tariff.h>
+#include <locale.h>
+
+
+/* Test the GObject properties on a tariff. */
+static void
+test_tariff_properties (void)
+{
+  g_autoptr(MwtTariff) tariff = NULL;
+  g_autoptr(GPtrArray) periods = g_ptr_array_new_with_free_func (NULL);
+  g_autoptr(GDateTime) period_start = g_date_time_new_utc (2018, 1, 22, 0, 0, 0);
+  g_autoptr(GDateTime) period_end = g_date_time_new_utc (2018, 2, 22, 0, 0, 0);
+  g_autoptr(MwtPeriod) period = mwt_period_new (period_start, period_end,
+                                                MWT_PERIOD_REPEAT_NONE, 0,
+                                                NULL);
+  g_ptr_array_add (periods, period);
+  tariff = mwt_tariff_new ("name", periods);
+  g_assert_true (MWT_IS_TARIFF (tariff));
+
+  g_assert_cmpstr (mwt_tariff_get_name (tariff), ==, "name");
+  GPtrArray *actual_periods = mwt_tariff_get_periods (tariff);
+  g_assert_cmpuint (actual_periods->len, ==, 1);
+
+  g_autofree gchar *actual_name = NULL;
+  g_autoptr(GPtrArray) actual_periods2 = NULL;
+
+  g_object_get (G_OBJECT (tariff),
+                "name", &actual_name,
+                "periods", &actual_periods2,
+                NULL);
+
+  g_assert_cmpstr (actual_name, ==, "name");
+  g_assert_cmpuint (actual_periods2->len, ==, 1);
+}
+
+/* Test mwt_tariff_lookup_period() with various dates/times for a generic
+ * tariff. */
+static void
+test_tariff_lookup (void)
+{
+  /* Construct a tariff. */
+  g_autoptr(GPtrArray) periods = g_ptr_array_new_with_free_func (NULL);
+
+  g_autoptr(GDateTime) period1_start = g_date_time_new_utc (2018, 1, 22, 0, 0, 0);
+  g_autoptr(GDateTime) period1_end = g_date_time_new_utc (2018, 2, 22, 0, 0, 0);
+  g_autoptr(MwtPeriod) period1 = mwt_period_new (period1_start, period1_end,
+                                                 MWT_PERIOD_REPEAT_NONE, 0,
+                                                 "capacity-limit", 2000,
+                                                 NULL);
+  g_ptr_array_add (periods, period1);
+
+  g_autoptr(GDateTime) period2_start = g_date_time_new_utc (2018, 1, 22, 0, 0, 0);
+  g_autoptr(GDateTime) period2_end = g_date_time_new_utc (2018, 1, 22, 12, 0, 0);
+  g_autoptr(MwtPeriod) period2 = mwt_period_new (period2_start, period2_end,
+                                                 MWT_PERIOD_REPEAT_WEEK, 1,
+                                                 "capacity-limit", 1000,
+                                                 NULL);
+  g_ptr_array_add (periods, period2);
+
+  g_autoptr(MwtTariff) tariff = NULL;
+  tariff = mwt_tariff_new ("name", periods);
+
+  /* Check whether periods are looked up correctly. */
+  const struct
+    {
+      gint year;
+      gint month;
+      gint day;
+      gint hour;
+      gint minute;
+      gdouble seconds;
+      MwtPeriod *expected_period;
+    }
+  vectors[] =
+    {
+      { 2018, 1, 21, 23, 59, 59, NULL },
+      { 2018, 1, 22, 0, 0, 0, period2 },
+      { 2018, 1, 22, 1, 0, 0, period2 },
+      { 2018, 1, 22, 11, 59, 59, period2 },
+      { 2018, 1, 22, 12, 0, 0, period1 },
+      { 2018, 1, 22, 12, 0, 1, period1 },
+      { 2018, 1, 23, 0, 0, 0, period1 },
+      { 2018, 2, 21, 23, 59, 59, period1 },
+      { 2018, 2, 22, 0, 0, 0, NULL },
+      /* FIXME: Test recurrence */
+    };
+
+  for (gsize i = 0; i < G_N_ELEMENTS (vectors); i++)
+    {
+      g_test_message ("Vector %" G_GSIZE_FORMAT ": %04d-%02d-%02dT%02d:%02d:%02fZ, %p",
+                      i, vectors[i].year, vectors[i].month, vectors[i].day,
+                      vectors[i].hour, vectors[i].minute, vectors[i].seconds,
+                      vectors[i].expected_period);
+      g_autoptr(GDateTime) lookup_date =
+          g_date_time_new_utc (vectors[i].year, vectors[i].month, vectors[i].day,
+                               vectors[i].hour, vectors[i].minute, vectors[i].seconds);
+      MwtPeriod *lookup_period = mwt_tariff_lookup_period (tariff, lookup_date);
+      if (vectors[i].expected_period == NULL)
+        {
+          g_assert_null (lookup_period);
+        }
+      else
+        {
+          g_assert_true (MWT_IS_PERIOD (lookup_period));
+          g_assert_true (lookup_period == vectors[i].expected_period);
+        }
+    }
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  setlocale (LC_ALL, "");
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/tariff/properties", test_tariff_properties);
+  g_test_add_func ("/tariff/lookup", test_tariff_lookup);
+
+  return g_test_run ();
+}
