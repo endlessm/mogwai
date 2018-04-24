90a214171211a4febbb503a03796c6a9496d5460
Philip Withnall <withnall@endlessm.com>
2018-04-24 14:13:26 +0100

    libmogwai-schedule: Add tests for MwsScheduleService D-Bus API
    This includes dummy MwsConnectionMonitor and MwsPeerManager
    implementations which can be used by the test harness.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T22164

---
+++ libmogwai-schedule/tests/peer-manager-dummy.h
@@ -0,0 +1,40 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright © 2018 Endless Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Authors:
+ *  - Philip Withnall <withnall@endlessm.com>
+ */
+
+#pragma once
+
+#include <glib.h>
+#include <glib-object.h>
+#include <libmogwai-schedule/peer-manager.h>
+
+G_BEGIN_DECLS
+
+#define MWS_TYPE_PEER_MANAGER_DUMMY mws_peer_manager_dummy_get_type ()
+G_DECLARE_FINAL_TYPE (MwsPeerManagerDummy, mws_peer_manager_dummy, MWS, PEER_MANAGER_DUMMY, GObject)
+
+MwsPeerManagerDummy *mws_peer_manager_dummy_new      (gboolean             fail);
+
+gboolean             mws_peer_manager_dummy_get_fail (MwsPeerManagerDummy *self);
+void                 mws_peer_manager_dummy_set_fail (MwsPeerManagerDummy *self,
+                                                      gboolean             fail);
+
+G_END_DECLS
